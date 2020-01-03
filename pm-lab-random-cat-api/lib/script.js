c1119639fdbbd8d53c15558df5a5e51086c452c8
rixiobarrios <rixiobarrios.consulado@gmail.com>
2020-01-02 20:29:44 -0600

    Rixio Barrios Random Cat API Homework

--- pm-lab-random-cat-api/lib/script.js
+++ pm-lab-random-cat-api/lib/script.js
@@ -5,11 +5,35 @@ let handleRandomCat = function() {
             'x-api-key': '34e7bfbf-78c6-4be3-a091-45ea9b905500'
         }
     })
-        .then(response => response.json())
-        .then(response => {
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
             randomCatImage.src = `${response[0].url}`;
         });
 };
 
 const randomCatButton = document.querySelector('.randomButton');
 randomCatButton.addEventListener('click', handleRandomCat);
+
+let handleRandomCategoryCat = function(evt) {
+    evt.preventDefault();
+    const randomCategoryImage = document.querySelector('.categoryCatImage');
+    const userInput = document.querySelector('.input').value;
+    fetch(
+        `https://api.thecatapi.com/v1/images/search?category_ids=${userInput}`,
+        {
+            headers: {
+                'x-api-key': '34e7bfbf-78c6-4be3-a091-45ea9b905500'
+            }
+        }
+    )
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            randomCategoryImage.src = `${response[0].url}`;
+        });
+};
+const findCatsMeow = document.querySelector('.searchButton');
+findCatsMeow.addEventListener('click', handleRandomCategoryCat);
